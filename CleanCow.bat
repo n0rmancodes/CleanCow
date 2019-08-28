f046a3850c1aad6ac6c3c33cac8e16166e0b95f5
Norman <41760554+n0rmancodes@users.noreply.github.com>
2019-08-28 10:03:01 -0400

    Update CleanCow.bat

--- CleanCow.bat
+++ CleanCow.bat
@@ -1,6 +1,6 @@
 @echo off
-title CleanCow [v1.2.2]
-echo CleanCow [v1.2.2]
+title CleanCow [v1.3.0]
+echo CleanCow [v1.3.0]
 echo ============================
 echo Detecting admin privileges...
 net session >nul 2>&1
@@ -15,35 +15,97 @@ cls
 echo Loading... [Starting timer.]
 cls
 set STARTTIME=%TIME%
-echo CleanCow [v1.2.2]
+echo CleanCow [v1.3.0]
 echo ============================
 echo Removing useless files... (*.tmp)
 echo - Please wait... 
 del /f /s /q %systemdrive%\*.tmp >nul
 cls
-echo CleanCow [v1.2.2]
-echo ============================
-echo Removing useless files... (*.tmp)
-echo - Successfully removed
-echo Removing useless files... (*._mp)
-echo - Please wait... 
-del /f /s /q %systemdrive%\*._mp >nul
-cls
-echo CleanCow [v1.2.2]
+echo CleanCow [v1.3.0]
 echo ============================
 echo Removing useless files... (*.tmp)
 echo - Successfully removed
 echo Removing useless files... (*._mp)
 echo - Successfully removed!
-echo Removing useless files... (*.log)
+echo Removing useless files... (*.temp)
 echo - Please wait... 
-del /f /s /q %systemdrive%\*.log >nul
+del /f /s /q %systemdrive%\*.temp >nul
 cls
-echo CleanCow [v1.2.2]
+echo CleanCow [v1.3.0]
 echo ============================
 echo Removing useless files... (*.tmp)
 echo - Successfully removed
 echo Removing useless files... (*._mp)
+echo - Successfully removed!
+echo Removing useless files... (*.temp)
+echo - Successfully removed!
+echo Removing useless files... (*.nch)
+echo - Please wait...
+del /f /s /q %systemdrive%\*.nch >nul
+cls
+echo CleanCow [v1.3.0]
+echo ============================
+echo Removing useless files... (*.tmp)
+echo - Successfully removed
+echo Removing useless files... (*._mp)
+echo - Successfully removed!
+echo Removing useless files... (*.temp)
+echo - Successfully removed!
+echo Removing useless files... (*.nch)
+echo - Successfully removed!
+echo Removing useless files... (*.wch)
+echo - Please wait...
+del /f /s /q %systemdrive%\*.wch >nul
+cls
+cls
+echo CleanCow [v1.3.0]
+echo ============================
+echo Removing useless files... (*.tmp)
+echo - Successfully removed
+echo Removing useless files... (*._mp)
+echo - Successfully removed!
+echo Removing useless files... (*.temp)
+echo - Successfully removed!
+echo Removing useless files... (*.nch)
+echo - Successfully removed!
+echo Removing useless files... (*.wch)
+echo - Successfully removed!
+echo Removing useless files... (*.fts, *.ftg)
+echo - Please wait...
+del /f /s /q %systemdrive%\*.fts >nul
+del /f /s /q %systemdrive%\*.ftg >nul
+cls
+echo CleanCow [v1.3.0]
+echo ============================
+echo Removing useless files... (*.tmp)
+echo - Successfully removed
+echo Removing useless files... (*._mp)
+echo - Successfully removed!
+echo Removing useless files... (*.temp)
+echo - Successfully removed!
+echo Removing useless files... (*.nch)
+echo - Successfully removed!
+echo Removing useless files... (*.wch)
+echo - Successfully removed!
+echo Removing useless files... (*.fts, *.ftg)
+echo - Successfully removed!
+echo Removing useless files... (*.log)
+echo - Please wait... 
+del /f /s /q %systemdrive%\*.log >nul
+cls
+echo CleanCow [v1.3.0]
+echo ============================
+echo Removing useless files... (*.tmp)
+echo - Successfully removed
+echo Removing useless files... (*._mp)
+echo - Successfully removed!
+echo Removing useless files... (*.temp)
+echo - Successfully removed!
+echo Removing useless files... (*.nch)
+echo - Successfully removed!
+echo Removing useless files... (*.wch)
+echo - Successfully removed!
+echo Removing useless files... (*.fts, *.ftg)
 echo - Successfully removed!
 echo Removing useless files... (*.log)
 echo - Successfully removed!
@@ -51,29 +113,73 @@ echo Removing useless files... (*.gid)
 echo - Please wait... 
 del /f /s /q %systemdrive%\*.gid >nul
 cls
-echo CleanCow [v1.2.2]
+echo Removing useless files... (*.tmp)
+echo - Successfully removed
+echo Removing useless files... (*._mp)
+echo - Successfully removed!
+echo Removing useless files... (*.temp)
+echo - Successfully removed!
+echo Removing useless files... (*.nch)
+echo - Successfully removed!
+echo Removing useless files... (*.wch)
+echo - Successfully removed!
+echo Removing useless files... (*.fts, *.ftg)
+echo - Successfully removed!
+echo Removing useless files... (*.log)
+echo - Successfully removed!
+echo Removing useless files... (*.gid)
+echo - Successfully removed!
+echo Removing useless files... (*.$$$, *.err, *.—, *.~*, ~*.*, *.??$)
+del /f /s /q %systemdrive%\*.$$$ >nul
+del /f /s /q %systemdrive%\*.err >nul
+del /f /s /q %systemdrive%\*.— >nul
+del /f /s /q %systemdrive%\*.~* >nul
+del /f /s /q %systemdrive%\~*.* >nul
+del /f /s /q %systemdrive%\*.??$ >nul
+cls
+echo CleanCow [v1.3.0]
 echo ============================
 echo Removing useless files... (*.tmp)
 echo - Successfully removed
 echo Removing useless files... (*._mp)
 echo - Successfully removed!
+echo Removing useless files... (*.temp)
+echo - Successfully removed!
+echo Removing useless files... (*.nch)
+echo - Successfully removed!
+echo Removing useless files... (*.wch)
+echo - Successfully removed!
+echo Removing useless files... (*.fts, *.ftg)
+echo - Successfully removed!
 echo Removing useless files... (*.log)
 echo - Successfully removed!
 echo Removing useless files... (*.gid)
+echo - Successfully removed!
+echo Removing useless files... (*.$$$, *.err, *.—, *.~*, ~*.*, *.??$)
 echo - Successfully removed!
 echo Removing useless files... (*.chk)
 echo - Please wait... 
 del /f /s /q %systemdrive%\*.chk >nul
 cls
-echo CleanCow [v1.2.2]
+echo CleanCow [v1.3.0]
 echo ============================
 echo Removing useless files... (*.tmp)
 echo - Successfully removed
 echo Removing useless files... (*._mp)
 echo - Successfully removed!
+echo Removing useless files... (*.temp)
+echo - Successfully removed!
+echo Removing useless files... (*.nch)
+echo - Successfully removed!
+echo Removing useless files... (*.wch)
+echo - Successfully removed!
+echo Removing useless files... (*.fts, *.ftg)
+echo - Successfully removed!
 echo Removing useless files... (*.log)
 echo - Successfully removed!
 echo Removing useless files... (*.gid)
+echo - Successfully removed!
+echo Removing useless files... (*.$$$, *.err, *.—, *.~*, ~*.*, *.??$)
 echo - Successfully removed!
 echo Removing useless files... (*.chk)
 echo - Successfully removed!
@@ -81,15 +187,25 @@ echo Removing useless files... (*.old)
 echo - Please wait... 
 del /f /s /q %systemdrive%\*.old >nul
 cls
-echo CleanCow [v1.2.2]
+echo CleanCow [v1.3.0]
 echo ============================
 echo Removing useless files... (*.tmp)
 echo - Successfully removed
 echo Removing useless files... (*._mp)
 echo - Successfully removed!
+echo Removing useless files... (*.temp)
+echo - Successfully removed!
+echo Removing useless files... (*.nch)
+echo - Successfully removed!
+echo Removing useless files... (*.wch)
+echo - Successfully removed!
+echo Removing useless files... (*.fts, *.ftg)
+echo - Successfully removed!
 echo Removing useless files... (*.log)
 echo - Successfully removed!
 echo Removing useless files... (*.gid)
+echo - Successfully removed!
+echo Removing useless files... (*.$$$, *.err, *.—, *.~*, ~*.*, *.??$)
 echo - Successfully removed!
 echo Removing useless files... (*.chk)
 echo - Successfully removed!
@@ -99,15 +215,25 @@ echo Removing useless files... (*.bak)
 echo - Please wait... 
 del /f /s /q %windir%\*.bak >nul
 cls
-echo CleanCow [v1.2.2]
+echo CleanCow [v1.3.0]
 echo ============================
 echo Removing useless files... (*.tmp)
 echo - Successfully removed
 echo Removing useless files... (*._mp)
 echo - Successfully removed!
+echo Removing useless files... (*.temp)
+echo - Successfully removed!
+echo Removing useless files... (*.nch)
+echo - Successfully removed!
+echo Removing useless files... (*.wch)
+echo - Successfully removed!
+echo Removing useless files... (*.fts, *.ftg)
+echo - Successfully removed!
 echo Removing useless files... (*.log)
 echo - Successfully removed!
 echo Removing useless files... (*.gid)
+echo - Successfully removed!
+echo Removing useless files... (*.$$$, *.err, *.—, *.~*, ~*.*, *.??$)
 echo - Successfully removed!
 echo Removing useless files... (*.chk)
 echo - Successfully removed!
@@ -119,15 +245,25 @@ echo Removing prefetch folder files...
 echo - Please wait... 
 del /f /s /q %windir%\prefetch\*.* >nul
 cls
-echo CleanCow [v1.2.2]
+echo CleanCow [v1.3.0]
 echo ============================
 echo Removing useless files... (*.tmp)
 echo - Successfully removed
 echo Removing useless files... (*._mp)
 echo - Successfully removed!
+echo Removing useless files... (*.temp)
+echo - Successfully removed!
+echo Removing useless files... (*.nch)
+echo - Successfully removed!
+echo Removing useless files... (*.wch)
+echo - Successfully removed!
+echo Removing useless files... (*.fts, *.ftg)
+echo - Successfully removed!
 echo Removing useless files... (*.log)
 echo - Successfully removed!
 echo Removing useless files... (*.gid)
+echo - Successfully removed!
+echo Removing useless files... (*.$$$, *.err, *.—, *.~*, ~*.*, *.??$)
 echo - Successfully removed!
 echo Removing useless files... (*.chk)
 echo - Successfully removed!
@@ -141,15 +277,25 @@ echo Removing cookies and temporary internet files... (cookies folder)
 echo - Please wait... 
 del /f /q %userprofile%\cookies\*.* >nul
 cls
-echo CleanCow [v1.2.2]
+echo CleanCow [v1.3.0]
 echo ============================
 echo Removing useless files... (*.tmp)
 echo - Successfully removed
 echo Removing useless files... (*._mp)
 echo - Successfully removed!
+echo Removing useless files... (*.temp)
+echo - Successfully removed!
+echo Removing useless files... (*.nch)
+echo - Successfully removed!
+echo Removing useless files... (*.wch)
+echo - Successfully removed!
+echo Removing useless files... (*.fts, *.ftg)
+echo - Successfully removed!
 echo Removing useless files... (*.log)
 echo - Successfully removed!
 echo Removing useless files... (*.gid)
+echo - Successfully removed!
+echo Removing useless files... (*.$$$, *.err, *.—, *.~*, ~*.*, *.??$)
 echo - Successfully removed!
 echo Removing useless files... (*.chk)
 echo - Successfully removed!
@@ -165,15 +311,25 @@ echo Removing cookies and temporary internet files... (recent folder)
 echo - Please wait... 
 del /f /q %userprofile%\recent\*.* >nul
 cls
-echo CleanCow [v1.2.2]
+echo CleanCow [v1.3.0]
 echo ============================
 echo Removing useless files... (*.tmp)
 echo - Successfully removed
 echo Removing useless files... (*._mp)
 echo - Successfully removed!
+echo Removing useless files... (*.temp)
+echo - Successfully removed!
+echo Removing useless files... (*.nch)
+echo - Successfully removed!
+echo Removing useless files... (*.wch)
+echo - Successfully removed!
+echo Removing useless files... (*.fts, *.ftg)
+echo - Successfully removed!
 echo Removing useless files... (*.log)
 echo - Successfully removed!
 echo Removing useless files... (*.gid)
+echo - Successfully removed!
+echo Removing useless files... (*.$$$, *.err, *.—, *.~*, ~*.*, *.??$)
 echo - Successfully removed!
 echo Removing useless files... (*.chk)
 echo - Successfully removed!
@@ -191,15 +347,25 @@ echo Removing cookies and temporary internet files... (temp internet files)
 echo - Please wait... 
 del /f /s /q “%userprofile%\Local Settings\Temporary Internet Files\*.*” >nul
 cls
-echo CleanCow [v1.2.2]
+echo CleanCow [v1.3.0]
 echo ============================
 echo Removing useless files... (*.tmp)
 echo - Successfully removed
 echo Removing useless files... (*._mp)
 echo - Successfully removed!
+echo Removing useless files... (*.temp)
+echo - Successfully removed!
+echo Removing useless files... (*.nch)
+echo - Successfully removed!
+echo Removing useless files... (*.wch)
+echo - Successfully removed!
+echo Removing useless files... (*.fts, *.ftg)
+echo - Successfully removed!
 echo Removing useless files... (*.log)
 echo - Successfully removed!
 echo Removing useless files... (*.gid)
+echo - Successfully removed!
+echo Removing useless files... (*.$$$, *.err, *.—, *.~*, ~*.*, *.??$)
 echo - Successfully removed!
 echo Removing useless files... (*.chk)
 echo - Successfully removed!
@@ -219,15 +385,25 @@ echo Removing cookies and temporary internet files... (other temp folders)
 echo - Please wait... 
 del /f /s /q “%userprofile%\Local Settings\Temp\*.*” >nul
 cls
-echo CleanCow [v1.2.2]
+echo CleanCow [v1.3.0]
 echo ============================
 echo Removing useless files... (*.tmp)
 echo - Successfully removed
 echo Removing useless files... (*._mp)
 echo - Successfully removed!
+echo Removing useless files... (*.temp)
+echo - Successfully removed!
+echo Removing useless files... (*.nch)
+echo - Successfully removed!
+echo Removing useless files... (*.wch)
+echo - Successfully removed!
+echo Removing useless files... (*.fts, *.ftg)
+echo - Successfully removed!
 echo Removing useless files... (*.log)
 echo - Successfully removed!
 echo Removing useless files... (*.gid)
+echo - Successfully removed!
+echo Removing useless files... (*.$$$, *.err, *.—, *.~*, ~*.*, *.??$)
 echo - Successfully removed!
 echo Removing useless files... (*.chk)
 echo - Successfully removed!
@@ -245,15 +421,25 @@ echo Removing cookies and temporary internet files... (temp internet files)
 echo - Please wait... 
 del /f /s /q “%userprofile%\Local Settings\Temporary Internet Files\*.*” >nul
 cls
-echo CleanCow [v1.2.2]
+echo CleanCow [v1.3.0]
 echo ============================
 echo Removing useless files... (*.tmp)
 echo - Successfully removed
 echo Removing useless files... (*._mp)
 echo - Successfully removed!
+echo Removing useless files... (*.temp)
+echo - Successfully removed!
+echo Removing useless files... (*.nch)
+echo - Successfully removed!
+echo Removing useless files... (*.wch)
+echo - Successfully removed!
+echo Removing useless files... (*.fts, *.ftg)
+echo - Successfully removed!
 echo Removing useless files... (*.log)
 echo - Successfully removed!
 echo Removing useless files... (*.gid)
+echo - Successfully removed!
+echo Removing useless files... (*.$$$, *.err, *.—, *.~*, ~*.*, *.??$)
 echo - Successfully removed!
 echo Removing useless files... (*.chk)
 echo - Successfully removed!
@@ -276,15 +462,25 @@ del /f /s /q “%userprofile%\.bash_history” >nul
 del /f /s /q “%userprofile%\.gitconfig” >nul
 del /f /s /q “%userprofile%\.node_repl_history” >nul
 cls
-echo CleanCow [v1.2.2]
+echo CleanCow [v1.3.0]
 echo ============================
 echo Removing useless files... (*.tmp)
 echo - Successfully removed
 echo Removing useless files... (*._mp)
 echo - Successfully removed!
+echo Removing useless files... (*.temp)
+echo - Successfully removed!
+echo Removing useless files... (*.nch)
+echo - Successfully removed!
+echo Removing useless files... (*.wch)
+echo - Successfully removed!
+echo Removing useless files... (*.fts, *.ftg)
+echo - Successfully removed!
 echo Removing useless files... (*.log)
 echo - Successfully removed!
 echo Removing useless files... (*.gid)
+echo - Successfully removed!
+echo Removing useless files... (*.$$$, *.err, *.—, *.~*, ~*.*, *.??$)
 echo - Successfully removed!
 echo Removing useless files... (*.chk)
 echo - Successfully removed!
@@ -302,15 +498,25 @@ echo Removing cookies and temporary internet files... (temp internet files)
 echo - Please wait... 
 del /f /s /q “%userprofile%\Local Settings\Temporary Internet Files\*.*” >nul
 cls
-echo CleanCow [v1.2.2]
+echo CleanCow [v1.3.0]
 echo ============================
 echo Removing useless files... (*.tmp)
 echo - Successfully removed
 echo Removing useless files... (*._mp)
 echo - Successfully removed!
+echo Removing useless files... (*.temp)
+echo - Successfully removed!
+echo Removing useless files... (*.nch)
+echo - Successfully removed!
+echo Removing useless files... (*.wch)
+echo - Successfully removed!
+echo Removing useless files... (*.fts, *.ftg)
+echo - Successfully removed!
 echo Removing useless files... (*.log)
 echo - Successfully removed!
 echo Removing useless files... (*.gid)
+echo - Successfully removed!
+echo Removing useless files... (*.$$$, *.err, *.—, *.~*, ~*.*, *.??$)
 echo - Successfully removed!
 echo Removing useless files... (*.chk)
 echo - Successfully removed!
@@ -336,15 +542,25 @@ rd "Users\%userprofile%\AppData\Local\CrashRpt\UnsentCrashReports"
 rd "Users\%userprofile%\AppData\Local\D3DSCache"
 rd "Users\%userprofile%\AppData\Local\SquirrelTemp"
 cls
-echo CleanCow [v1.2.2]
+echo CleanCow [v1.3.0]
 echo ============================
 echo Removing useless files... (*.tmp)
 echo - Successfully removed
 echo Removing useless files... (*._mp)
 echo - Successfully removed!
+echo Removing useless files... (*.temp)
+echo - Successfully removed!
+echo Removing useless files... (*.nch)
+echo - Successfully removed!
+echo Removing useless files... (*.wch)
+echo - Successfully removed!
+echo Removing useless files... (*.fts, *.ftg)
+echo - Successfully removed!
 echo Removing useless files... (*.log)
 echo - Successfully removed!
 echo Removing useless files... (*.gid)
+echo - Successfully removed!
+echo Removing useless files... (*.$$$, *.err, *.—, *.~*, ~*.*, *.??$)
 echo - Successfully removed!
 echo Removing useless files... (*.chk)
 echo - Successfully removed!
@@ -362,6 +578,7 @@ echo Removing cookies and temporary internet files... (temp internet files)
 echo - Successfully removed!
 echo Removing useless folders... 
 echo - Successfully removed!
+echo -- [Finishing timer...]
 set ENDTIME=%TIME%
 for /F "tokens=1-4 delims=:.," %%a in ("%STARTTIME%") do (
        set /A "start=(((%%a*60)+1%%b %% 100)*60+1%%c %% 100)*100+1%%d %% 100"
@@ -376,8 +593,10 @@ if %mm% lss 10 set mm=0%mm%
 if %ss% lss 10 set ss=0%ss%
 if %cc% lss 10 set cc=0%cc%
 set DURATION=%hh%:%mm%:%ss%,%cc%
+goto endScreen
+:endScreen
 cls 
-echo CleanCow [v1.2.2] 
+echo CleanCow [v1.3.0] 
 echo ============================
 echo Started: %STARTTIME%
 echo Finished: %ENDTIME%
@@ -387,13 +606,14 @@ echo ============================
 echo Coded by n0rmancodes. Additional credits are found in the Github.
 echo This window will close in 20 seconds (or when a key is pressed).
 timeout /t 20 >nul
-end
+exit
 :admin
 cls
-title Error (CleanCow [v1.2.2])
-echo CleanCow [v1.2.2]
+title Error (CleanCow [v1.3.0])
+echo CleanCow [v1.3.0]
 echo ============================
 echo Error!
 echo This program requires admin privileges in order to clean most files. Please run this as admin when you relaunch this!
 echo This program will close in 20 seconds (or when a key is pressed).
 timeout /t 20 >nul
+exit
